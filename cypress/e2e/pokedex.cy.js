ca4b33a4867190f34a0a08f2805ad3886342459d
Nevena Turkovic <nevenatu@gmail.com>
2022-12-19 22:44:31 +0100

    eslint fixes

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,9 +1,18 @@
 describe('Pokedex', function () {
+  beforeEach(function () {
+    cy.visit('http://localhost:3000')
+  })
+
   it('front page can be opened', function () {
-    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+
+  it('one can navigate from the main page to the page of a particular Pokemon', function () {
+    cy.contains('ivysaur').click()
+    cy.visit('http://localhost:5000/pokemon/ivysaur')
+    cy.contains('chlorophyll')
   })
 })
