e428e8f3c7281c88784ce1d1ee3506286291b66a
Nevena Turkovic <nevenatu@gmail.com>
2022-12-20 21:57:56 +0100

    Wrote script ensuring health check endpoint works

--- health_checks/health_check.sh
+++ health_checks/health_check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+# echo "Hello from shell script"
+
+curl "https://$FLY_APP_NAME.fly.dev/health" | grep -q "ok"
 
 # exit 1 # exit status 1 means that the script "fails"
-exit 0 # exit status 0 means that the script "succeeds"
+#exit 0 # exit status 0 means that the script "succeeds"
